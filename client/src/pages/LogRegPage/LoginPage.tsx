2f1dfd29508dd99f44e973609ce346845a7dfe9c
Aygiz Khatipov <you@example.com>
2024-08-15 21:27:21 +0300

    createLogRegPage relise 1

--- client/src/pages/LogRegPage/LoginPage.tsx
+++ client/src/pages/LogRegPage/LoginPage.tsx
@@ -1,9 +1,58 @@
-import React from 'react'
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from '@mantine/core';
+import classes from './AunthenticationTitle.module.css';
+import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from "react-hook-form"
 
-function LoginPage(): JSX.Element {
-  return (
-<div>LoginPage</div>
-)
+export function AuthenticationTitle() {
+
+
+
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<{ login: string, password: string }>({
+    mode: 'onChange',
+});
+
+const onSubmit: SubmitHandler<{ login: string, password: string }> = (data) => {
+  console.log(data)
 }
 
-export default LoginPage
+
+  return (
+    <Container size={420} my={40}>
+      <Title ta="center" className={classes.title}>
+        Welcome back!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+        Do not have an account yet?{' '}
+        <Anchor size="sm" component="button">
+          <Link to="/auth/reg">Create account</Link>
+        </Anchor>
+      </Text>
+      <form  onSubmit={handleSubmit(onSubmit)}>
+      <Paper  withBorder shadow="md" p={30} mt={30} radius="md" >
+        <TextInput label="Login" placeholder="Login" required  {...register("login")}/>
+        <PasswordInput label="Password" placeholder="Your password" required mt="md" {...register("password")}/>
+        <Group justify="space-between" mt="lg">
+          <Anchor component="button" size="sm">
+          </Anchor>
+        </Group>
+        <Button fullWidth mt="md" type="submit"  size="md">
+          Sign in
+        </Button>
+      </Paper>
+      </form>
+    </Container>
+  );
+}
+
+export default AuthenticationTitle;
--- client/src/pages/LogRegPage/RegPage.tsx
+++ client/src/pages/LogRegPage/RegPage.tsx
@@ -1,8 +1,52 @@
-import React from 'react'
+import {
+  TextInput,
+  PasswordInput,
+  Checkbox,
+  Anchor,
+  Paper,
+  Title,
+  Text,
+  Container,
+  Group,
+  Button,
+} from '@mantine/core';
+import classes from './AunthenticationTitle.module.css';
+import { Link } from 'react-router-dom';
+import { SubmitHandler, useForm } from "react-hook-form"
 
 function RegPage(): JSX.Element {
+
+
+  const { register, handleSubmit, formState: { errors }, watch } = useForm<{ login: string, password: string }>({
+    mode: 'onChange',
+});
+
+const onSubmit: SubmitHandler<{ login: string, password: string }> = (data) => {
+  console.log(data)
+}
   return (
-<div>RegPage</div>
+<Container size={420} my={40}>
+      <Title ta="center" className={classes.title}>
+        Hi, new User!
+      </Title>
+      <Text c="dimmed" size="sm" ta="center" mt={5}>
+      Do you have an account?{' '}
+        <Anchor size="sm" component="button">
+          <Link to="/auth/login">
+              Login to account</Link>
+        </Anchor>
+      </Text>
+      <form  onSubmit={handleSubmit(onSubmit)}>
+      <Paper  withBorder shadow="md" p={30} mt={30} radius="md" >
+        <TextInput label="Login" placeholder="Login" required  {...register("login")}/>
+        <PasswordInput label="Password" placeholder="Your password" required mt="md" {...register("password")}/>
+        <PasswordInput label="Confirm Password" placeholder="Confirm password" required mt="md" {...register("password")}/>
+        <Button fullWidth mt="md" type="submit"  size="md">
+          Sign up
+        </Button>
+      </Paper>
+      </form>
+    </Container>
 )
 }
 
