e642467170d4f9652b77f4d7b93647270dc7e102
Aygiz Khatipov <you@example.com>
2024-08-15 16:33:02 +0300

    CreateRoute

--- server/db/seeders/20240815121855-QuestionsSeed.js
+++ server/db/seeders/20240815121855-QuestionsSeed.js
@@ -23,22 +23,40 @@ module.exports = {
         topicId: 1
       },
       {
-        question: 'What is JavaScript?',
-        correctAnswer: 'Programming language',
+        question: 'What is Typescript?',
+        correctAnswer: 'Programming language TS',
         point: 5,
         topicId: 2
       },
       {
-        question: 'What is DOM?',
-        correctAnswer: 'Document Object Model',
+        question: 'What is ssss?',
+        correctAnswer: 'qwerrr',
         point: 3,
         topicId: 2
       },
       {
-        question: 'What is HTML?',
-        correctAnswer: 'Hyper Text Markup Language',
+        question: 'What is SQL?',
+        correctAnswer: 'Structured Query Language',
         point: 4,
         topicId: 2
+      },
+      { 
+        question: 'What is React?',
+        correctAnswer: 'Library',
+        point: 5,
+        topicId: 3
+      },
+      {
+        question: 'What is Redux?',
+        correctAnswer: 'State management library',
+        point: 3,
+        topicId: 3
+      },
+      {
+        question: 'What is Vue?',
+        correctAnswer: 'Framework',
+        point: 4,
+        topicId: 3
       }
     ], {});
     
