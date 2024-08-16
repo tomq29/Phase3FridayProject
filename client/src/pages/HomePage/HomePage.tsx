c196a900015e7707e59f6d1c5b145e3bd5350d87
Aygiz Khatipov <you@example.com>
2024-08-16 14:46:35 +0300

    createHomePage release 1

--- client/src/pages/HomePage/HomePage.tsx
+++ client/src/pages/HomePage/HomePage.tsx
@@ -1,5 +1,33 @@
+import { Image, Container, Title, Button, Group, Text, List, ThemeIcon, rem } from '@mantine/core';
+import classes from './HeroBullers.module.css';
+import { useNavigate } from 'react-router-dom';
+
+
 function HomePage(): JSX.Element {
-  return <div>HomePage</div>;
+
+
+const navigate = useNavigate();
+
+
+
+  return (
+    <Container size='xl'>
+      <div className={classes.inner}>
+        <div className={classes.content}>
+          <Title className={classes.title}>
+            Press the  <span className={classes.highlight}>button </span>  <br /> 
+            to start the game
+          </Title>
+          <Group mt={25}>
+            <Button onClick={() => navigate('/game')} radius="xl" size="md" className={classes.control}>
+              button
+            </Button>
+          </Group>
+        </div>
+        <Image src='https://i.gifer.com/AnA.gif' className={classes.image} />
+      </div>
+    </Container>
+  )
 }
 
 export default HomePage;
